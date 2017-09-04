dab63c19f91df3609f2b4edf2fa4f7bc711c7d72
Jared Jones <jkjones.dev@gmail.com>
2017-09-04 17:02:41 -0500

    removing history

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,6 @@ import BlogPost from '../blog/BlogPost.vue'
 Vue.use(Router)
 
 export default new Router({
-    mode: 'history',
     routes: [
         { path: '/', name: 'Home', component: Home },
         { path: '/blog/:id', component: BlogPost }

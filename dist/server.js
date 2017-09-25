185dfad7d1ba16be4832c66ca880fa55dffc5805
Jared Jones <jkjones.dev@gmail.com>
2017-09-25 09:02:07 -0500

    Deploy history mode

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,13 @@
-var express = require('express');
-var path = require('path');
+var history = require('connect-history-api-fallback');
+var express = require('express')
+var path = require('path')
 var serveStatic = require('serve-static')
-app = express();
+
+app = express()
+//add this middleware
+app.use(history());    
+app.use(serveStatic(__dirname))
 app.use(serveStatic(path.join(__dirname)));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started ' + port);
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started '+ port) 
